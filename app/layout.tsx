fafee013178d32cf917a67e88bc1a0f90d4ef571
Thiago Souza <tpimentel368@gmail.com>
2026-04-01 23:17:58 -0300

    feat: force dark mode to match original styling vision

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} min-w-screen min-h-screen antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} min-w-screen min-h-screen antialiased dark`}
     >
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
